15f232d2b21b2d8607e737b60542a393994b6128
brislazaro <brisa.lazaroc@gmail.com>
2024-06-03 19:11:37 +0200

    Switch to cors using dependency

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
 
+server.use(cors());
 server.use(jsonServer.defaults());
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 server.use(router);
 
