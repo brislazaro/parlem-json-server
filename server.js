426a39035d6efe44ae005f70824859dda1bac873
brislazaro <brisa.lazaroc@gmail.com>
2024-06-03 19:05:22 +0200

    Add cors header

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const router = jsonServer.router("db.json");
 
 server.use(jsonServer.defaults());
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(router);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}.`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
