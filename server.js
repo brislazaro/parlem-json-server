f9f474943833712eefa373da8f490326f4ffb313
brislazaro <brisa.lazaroc@gmail.com>
2024-06-03 18:58:50 +0200

    a

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ server.use(router);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}.`);
 });
